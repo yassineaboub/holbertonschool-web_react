abda3e96d1dd208d6391dc21a1a0c9b35997066d
yassineaboub <yassineaboub@live.fr>
2021-09-18 12:26:41 +0100

    task_2

---
+++ 0x02-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from './utils';
+import closeIcon from './close-icon.png';
+
+export const Notification = () => {
+  return (
+    <div className='Notifications'>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority='default'>New course available</li>
+        <li data-priority='urgent'>New resume available</li>
+        <li
+          data-priority='urgent'
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+      <button
+        className='close-icon'
+        aria-label='Close'
+        onClick={() => console.log('Close button has been clicked')}
+      >
+        <img
+          src={closeIcon}
+          alt='Close'
+          style={{ height: '20px', width: '20px' }}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default Notification;
